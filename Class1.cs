f6fb8ef79578b44dc943db406379ebdf8b8f5e7f
Yennin Muñoz <yarethgracia@gmail.com>
2020-10-19 19:24:40 -0700

    Agregar archivos de proyecto.

---
+++ Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperacionesCalculadora
+{
+    public class operaciones
+    {
+
+        public static double division( double n1, double n2)
+        {
+            double o;
+            o = n1 / n2;
+            return o;
+        }
+
+        public static double multiplicacion(double n1, double n2)
+        {
+            double o;
+            o = n1 * n2;
+            return o;
+        }
+
+        public static double resta (double n1, double n2)
+        {
+            double o;
+            o = n1 - n2;
+            return o;
+        }
+
+        public static double suma(double n1, double n2)
+        {
+            double o;
+            o = n1 / n2;
+            return o;
+        }
+
+    }
+}
